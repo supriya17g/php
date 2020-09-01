6325c8701ab2ba76fb98273c25f6881fa33bf432
ashutoshbhakare <bhakareashutosh@yahoo.com>
2020-09-01 14:45:06 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,11 +2,11 @@
 <html>
 <body>
 
-<h1>My first PHP page for WEBHOOKS v2.1 BYNIKHIL and AMAN</h1>
+<h1>My first PHP page</h1>
 
 <?php
 echo "Hello World!";
-echo "Hello from version1.1 UNNNATI for IBM GUYS ";
+echo "Hello from version1.1  ";
 echo " Why to Worry ? S2I in openshift";
 ?>
 
