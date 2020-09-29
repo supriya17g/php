7494ba7741a6459f447ebda0c067e4b6ff73f8f8
ashutoshbhakare <bhakareashutosh@yahoo.com>
2020-09-29 21:47:30 +0530

    Update index.php

--- index.php
+++ index.php
@@ -2,12 +2,11 @@
 <html>
 <body>
 
-<h1>My first PHP page for NOKIAGUYS v4.1 FINAL FINAL HURRAY </h1>
+<h1>WebHooks DEMO</h1>
 
 <?php
 echo "Hello World! from GIT";
 echo "Hello from version1.1  ";
-echo " Why to Worry ? S2I in openshift";
 ?>
 
 </body>
